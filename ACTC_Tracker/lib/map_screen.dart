1cd3b2e65246a398996333f42f509963707bd079
Phillip Pham <masterphillip17@gmail.com>
2020-04-24 22:58:01 -0400

    commit1
    commit

--- ACTC_Tracker/lib/map_screen.dart
+++ ACTC_Tracker/lib/map_screen.dart
@@ -1,4 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class 
+class MapScreen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _MapScreenState();
+}
+
+
+
+class _MapScreenState extends State<MapScreen>{
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(28, 35, 38, 100),
+      body: _buildMap()
+      appBar: _buildAppBar(),
+    ); //Scaffold
+  }
+
+  AppBar _buildAppBar(){
+    return AppBar(
+      backgroundColor: Colors.white,
+      title: Text(
+        "Map".toUpperCase(),
+        style: TextStyle(fontSize: 19.0, color: Colors.black87),
+      ),
+    ); //AppBar
+  }
+
+  LatLng _center = LatLng(40.762681, -73.832611)
+
+  FlutterMap _buildMap()
+  {
+    return FlutterMap(
+      options: new MapOptions(
+        minZoom: 15.0,
+        center: _center,
+        interactive: true,
+      ),
+
+      
+    )
+  }
+
+}
