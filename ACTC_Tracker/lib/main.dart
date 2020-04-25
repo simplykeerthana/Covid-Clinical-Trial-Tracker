029e3c4f2cf375451281a1395f265650630bf7f9
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-04-24 22:05:13 -0400

    commit

--- ACTC_Tracker/lib/main.dart
+++ ACTC_Tracker/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'map_screen.dart'
+import 'map_screen.dart';
 
 void main() {
   runApp(MyApp());
--- ACTC_Tracker/lib/map_screen.dart
+++ ACTC_Tracker/lib/map_screen.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class 
