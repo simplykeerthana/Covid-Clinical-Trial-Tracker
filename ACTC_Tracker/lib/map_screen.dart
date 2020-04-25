9f80e09c2b386fb6bde68ef85eea05f73efbb21a
Phillip Pham <masterphillip17@gmail.com>
2020-04-25 02:01:22 -0400

    Finally Working

--- ACTC_Tracker/lib/map_screen.dart
+++ ACTC_Tracker/lib/map_screen.dart
@@ -1,8 +1,8 @@
 
 import 'package:flutter/material.dart';
-import 'package:google_maps/google_maps.dart' hide Icon;
-import 'dart:html';
-import 'dart:ui' as ui;
+//import 'package:google_maps/google_maps.dart' hide Icon;
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'locations.dart' as locations;
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key key, this.title}) : super(key: key);
@@ -22,6 +22,46 @@ class MapScreen extends StatefulWidget {
   _MapScreenState createState() => _MapScreenState();
 }
 
+class  _MapScreenState extends State<MapScreen> {
+  final Map<String, Marker> _markers = {};
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    final googleOffices = await locations.getGoogleOffices();
+    setState(() {
+      _markers.clear();
+      for (final office in googleOffices.offices) {
+        final marker = Marker(
+          markerId: MarkerId(office.name),
+          position: LatLng(office.lat, office.lng),
+          infoWindow: InfoWindow(
+            title: office.name,
+            snippet: office.address,
+          ),
+        );
+        _markers[office.name] = marker;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Google Office Locations'),
+            backgroundColor: Colors.green[700],
+          ),
+          body: GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: const LatLng(0, 0),
+              zoom: 2,
+            ),
+            markers: _markers.values.toSet(),
+          ),
+        ),
+      );
+}
+
+/*
 class _MapScreenState extends State<MapScreen> {
 
   @override
@@ -78,3 +118,4 @@ class _MapScreenState extends State<MapScreen> {
     
   }
 }
+*/
